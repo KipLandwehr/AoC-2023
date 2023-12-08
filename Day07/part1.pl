4e1815757419c5e64efa702da425935edd4f5461
Kip Landwehr <klandwehr@live.com>
2023-12-08 12:11:52 -0600

    Add Day07

---
+++ Day07/part1.pl
@@ -0,0 +1,144 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my $debug = 0;
+my $total = 0;
+
+#	32T3K 765
+#	T55J5 684
+#	KK677 28
+#	KTJJT 220
+#	QQQJA 483
+
+my %letterValues = (
+	A => 14,
+	K => 13,
+	Q => 12,
+	J => 11,
+	T => 10,
+);
+
+my @hands;
+
+while (<>) {
+	chomp;
+	my ($hand, $bid) = split ( /\s+/ );
+	my @cards = split ( //, $hand );
+	foreach my $index ( 0..(@cards-1) ) {
+		if ( $cards[$index] =~ /[AKQJT]/ ) {
+			$cards[$index] = $letterValues{$cards[$index]};
+		}
+	}
+
+	if ( $debug > 4 ) {
+		print "Line $.: [ ";
+		foreach my $card ( @cards ) {
+			print "$card ";
+		}
+		print "], $bid\n";
+	}
+
+	if ( $debug > 4 ) {
+		my $type = getType(\@cards);
+		print "Line $.: Type = $type\n\n";
+		push ( @hands, [\@cards, $type, $bid] );
+	}
+	else {
+		push ( @hands, [\@cards, getType(\@cards), $bid] );
+	}
+}
+
+@hands = sort handSort @hands;
+
+if ( $debug > 0 ) {
+	print "Sorted Hands:\n";
+	foreach my $hIndex ( 0..(@hands-1) ) {
+		my @cards = @{$hands[$hIndex][0]};
+		print "[ ";
+		foreach my $card ( @cards ) {
+			print "$card ";
+		}
+		print "], $hands[$hIndex][1], $hands[$hIndex][2]\n";
+	}
+}
+
+foreach my $index ( 0..(@hands-1) ) {
+	my $bid = $hands[$index][2];
+	$total += $bid * ($index+1);
+}
+
+# Print solution
+print "Solution: $total\n";
+
+sub handSort {
+	# $a and $b will be array refs containing (hand, handType, bid)
+	my @h1 = @{$a};
+	my @h2 = @{$b};
+
+	my $h1Type = $h1[1];
+	my $h2Type = $h2[1];
+
+	my $compVal = $h1Type <=> $h2Type;
+
+	if ( $compVal == 0 ) {
+		# Check hand contents
+		my @h1Cards = @{$h1[0]};
+		my @h2Cards = @{$h2[0]};
+
+		for my $index ( 0..(@h1Cards-1) ) {
+			my $retVal = $h1Cards[$index] <=> $h2Cards[$index];
+			next if ( $retVal == 0 );
+			return $retVal;
+		}
+	}
+	return $compVal;
+}
+
+sub getType {
+	#	6 - Five of a kind: AAAAA
+	#	5 - Four of a kind: AA8AA
+	#	4 - Full house: 23332
+	#	3 - Three of a kind: TTT98
+	#	2 - Two pair: 23432
+	#	1 - One pair: A23A4
+	#	0 - High card: 23456
+
+	print "Executing getType\n" if ( $debug > 5 );
+
+	my $aRef = shift;
+	my @cards = @{$aRef};
+
+	if ( $debug > 6 ) {
+		print "getType: Card values - [ ";
+		foreach my $card (@cards) { print "$card "; }
+		print "]\n";
+	}
+
+	my %counts;
+
+	# count number of each card
+	foreach my $card (@cards) { ++$counts{$card};}
+
+	# sort puts higher values at higher indicies, so reverse sort
+	my @sortedCount = reverse sort ( values %counts );
+	
+	if ( $debug > 6 ) {
+		print "getType: sortedCount = [ ";
+		foreach my $count ( @sortedCount ) { print "$count "; }
+		print "]\n";
+	}
+
+	my $firstCount = shift @sortedCount;	
+
+	return 6 if ( $firstCount == 5 );
+	return 5 if ( $firstCount == 4 );
+	return 0 if ( $firstCount == 1 );
+
+	my $secondCount = shift @sortedCount;
+
+	return 4 if ( $firstCount == 3 and $secondCount == 2 );
+	return 3 if ( $firstCount == 3 and $secondCount == 1 );
+	return 2 if ( $firstCount == 2 and $secondCount == 2 );
+	return 1 if ( $firstCount == 2 and $secondCount == 1 );
+}
